7ca3592cb56f15ef90b82606f2611db1f03f797a
Fabricio <fabriciolak12@gmail.com>
2025-11-18 12:21:27 -0300

    v.6

--- program.c
+++ program.c
@@ -121,14 +121,56 @@ void program_div(RAM* ram, Register* reg, int dividend, int divisor) {
   }
 }
 
+// n (n × (n-1) × (n-2) × ... × 2 × 1).
+void program_fat(RAM* ram, Register* reg, int n) {
+  Instruction inst[MEMORY_SIZE] = {0};
+
+  set_ram(ram, 0, 1);  // RAM[0]: resultado acumulado (começa em 1)
+  set_ram(ram, 1, n);  // RAM[1]: contador (começa em n)
+  set_ram(ram, 2, 1);  // RAM[2]: constante 1
+
+  int pc = 0;
+  inst[pc++] = (Instruction){SUB, 1, 2,
+                             5};  // RAM[5] (ou RAM lixo), AC = RAM[1] - RAM[2]
+  inst[pc++] =
+      (Instruction){JLT, 5, 0, 5};  // Se AC < 0, salta pra HALT (linha 5)
+  inst[pc++] = (Instruction){MUL, 0, 1, 0};   // resultado *= contador
+  inst[pc++] = (Instruction){SUB, 1, 2, 1};   // contador -= 1
+  inst[pc++] = (Instruction){JUMP, 0, 0, 0};  // volta para o início
+  inst[pc++] = (Instruction){HALT, 0, 0, 0};  // fim
+
+  reg->PC = 0;
+  reg->IR = 0;
+
+  while (reg->IR != HALT && reg->PC < MEMORY_SIZE) {
+    execute_cpu(reg, ram, inst);
+  }
+
+  printf("Fatorial de %d = %d\n", n, get_ram(ram, 0));
+}
+
 int main(void) {
   Register reg = {0, 0, 0, 0, 0};
   RAM* ram = create_empty_ram(MEMORY_SIZE);
 
+  // PROGRAM: 1
+  // program_mult(ram, &reg, 10, 10);
+  // printf("Resultado = %d\n", get_ram(ram, 0));
+
+  // PROGRAM: 2
   // program_fibonacci(ram, &reg, 10);
-  program_sum_matrix(ram, &reg, 3);
-  program_div(ram, &reg, 10, 2);
+  // printf("Resultado = %d\n", get_ram(ram, 0));
+
+  // PROGRAM: 3
+  // program_sum_matrix(ram, &reg, 3);
+
+  // PROGRAM: 4
+  // program_div(ram, &reg, 10, 2);
   // printf("Resultado = %d\n", get_ram(ram, 3));
+
+  // PROGRAM: 5
+  // program_fat(ram, &reg, 10);
+  // printf("Resultado = %d\n", get_ram(ram, 2));
 
   destroy_ram(ram);
   return 0;
--- program.h
+++ program.h
@@ -6,9 +6,8 @@
 
 void program_mult(RAM* ram, Register* reg, int multiplicand, int multiplier);
 void program_div(RAM* ram, Register* reg, int dividend, int divisor);
-// void program_fat(RAM* ram, Register* reg);
+void program_fat(RAM* ram, Register* reg, int n);
 void program_sum_matrix(RAM* ram, Register* reg, int size);
-
 void program_fibonacci(RAM* ram, Register* reg, int term);
 
 #endif  // PROGRAM_H
